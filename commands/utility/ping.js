bb6027af898e86f076d23563828bb84b6092188e
cheater111 <79743387+cheater111@users.noreply.github.com>
2021-04-12 01:18:36 +0300

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -11,7 +11,7 @@ module.exports = {
 
 		const pingEmbed = new MessageEmbed()
 		.setAuthor(`${message.author.username}`, message.author.displayAvatarURL({format:"png", dynamic:true}))
-		.setThumbnail('https://cdn.discordapp.com/attachments/829429799549403136/829652374544056360/thing.png')
+		.setThumbnail('https://cdn.discordapp.com/attachments/825490227493732362/830929692847636540/91432dd928c8125aa2ad70660ccb3a94.png')
 		.addFields(
 			{ name: 'API', value: `${message.client.ws.ping}`},
 			{ name: 'Bot Latency', value: `${Date.now() - message.createdTimestamp}`}
