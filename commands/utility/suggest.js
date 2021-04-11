f1250da9ee0f14ae1c50281da021ff705f175211
cheater111 <79743387+cheater111@users.noreply.github.com>
2021-04-12 00:40:02 +0300

    Update suggest.js

--- commands/utility/suggest.js
+++ commands/utility/suggest.js
@@ -1,21 +1,26 @@
-const discord = require("discord.js");
-
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "suggest",
-  usage: " ",
-  description: "Suggest",
-  run: async (client, message, args) => {
-    const args = msg.content.slice(prefix.length).split(' '); 
-    const command = args.shift().toLowerCase();  
-    
-    
-    if (command === 'suggest'){   //if command is suggest
-    const channel = msg.guild.channels.find(ch => ch.name === 'suggestions');  //finds the channel named suggestions 
-    
-    channel.send('Suggestion:\n ' + args.join(' '))  //Sends the arguments
-    }     //Closes the if (command === 'suggest'){ 
-    }   //Closes the client.on('message',msg => {
+    name: 'suggestion',
+    description: 'My Info',
+    aliases: ['suggest','suggestions'],
+    guildOnly: true,
+    execute(message, args, cmd, client, Discord) {
+       
+        const content = args.splice(0).join(" ");
+        const channel = message.guild.channels.cache.find(channel => channel.name === "suggestions");
+        const icon = message.guild.iconURL({format: "png", size: 1024, dynamic: true})
+        const suggestionEmbed = new MessageEmbed()
+        .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({format: "png", size: 1024, dynamic: true}))
+        .setTitle('Suggestion')
+        .setColor(`#f4c93a`)
+        .setDescription(content)
+        .setTimestamp()
+        .setFooter(`${message.guild.name}`, `${icon}`)
 
-    
-}
+        channel.send(suggestionEmbed).then((message) =>{
+          message.react('✅');
+          message.react('❎');
+        });
+
+    }}
