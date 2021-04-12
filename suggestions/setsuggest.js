4ef4ac289aded139c8956d4cb3a0a2a81b3687d3
crt-romain78 <75182534+crt-romain78@users.noreply.github.com>
2021-04-12 08:52:41 +0200

    set suggestion channel
    set the suggestion channel
    w!setsuggest #channel

---
+++ suggestions/setsuggest.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+    name: "setsuggest",
+    category: "config",
+    usage: "setsuggest <#channel>",
+    authorPermission: ["MANAGE_GUILD"],
+    run: async (client, message, args) => {
+      if (!message.member.hasPermission("MANAGE_ROLES")) {
+      return message.channel.send(" ***sorry you need permission ``MANAGE_GUILD`` to do that***");
+    }
+
+        let Channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+
+        if (!Channel) return message.channel.send(` Please Mention A Channel!`);
+
+        if (Channel.type === "voice") return message.channel.send(` Please Mention A Text Channel!`);
+
+        await db.set(`suggestion_${message.guild.id}`, Channel.id);
+
+        let Embed = new MessageEmbed()
+        .setColor("00FFFF")
+        .setDescription(` Suggestion Channel is setted as <#${Channel.id}>`)
+
+        return message.channel.send(Embed);
+
+    }
+};
