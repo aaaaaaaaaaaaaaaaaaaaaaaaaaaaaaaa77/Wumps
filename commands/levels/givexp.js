149b7be04c96766eb78089783023f4e6ef433e47
Reece_ <67838925+ReeceTheDev@users.noreply.github.com>
2021-04-13 11:17:17 +0100

    Create givexp.js

---
+++ commands/levels/givexp.js
@@ -0,0 +1,38 @@
+const { getUserById, updateUserById } = require("../../utils/functions");
+
+module.exports = {
+  name: "givexp",
+  description: "Give someone Xp",
+  category: "levels",
+  usage: "givexp <amount> <user>",
+  memberPermissions: ["MANAGE_MEMBERS"],
+  async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const amount = args[0];
+    const member = bot.findMember(message, args);
+
+    if (!member) {
+      return message.channel.send(lang.MEMBER.PROVIDE_MEMBER);
+    }
+
+    if (!amount) {
+      return message.channel.send(lang.LEVELS.PROVIDE_AMOUNT);
+    }
+
+    if (isNaN(Number(amount))) {
+      return message.channel.send(lang.LEVELS.PROVIDE_VALID_NR);
+    }
+    const { user } = await getUserById(member.id, message.guild.id);
+
+    await updateUserById(member.id, message.guild.id, {
+      xp: user.xp + Number(amount),
+    });
+
+    message.channel.send(
+      lang.LEVELS.GIVE_XP_SUCCESS.replace(
+        "{member}",
+        member.user.tag
+      ).replace("{amount}", amount)
+    );
+  },
+};
